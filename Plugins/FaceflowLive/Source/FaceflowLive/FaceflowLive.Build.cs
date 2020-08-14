68a672f748345f363cb94ed0d6d57b40a64d7d84
David Hurtubise <david.hurtubise@epicgames.com>
2020-08-14 16:25:34 -0400

    Creating ThirdParty Lib and buid.cs path include

--- Plugins/FaceflowLive/Source/FaceflowLive/FaceflowLive.Build.cs
+++ Plugins/FaceflowLive/Source/FaceflowLive/FaceflowLive.Build.cs
@@ -1,14 +1,42 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class FaceflowLive : ModuleRules
 {
+	private string ThirdPartyPath
+        {
+            get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../ThirdParty/")); }
+        }
+		
 	public FaceflowLive(ReadOnlyTargetRules Target) : base(Target)
 	{
+		
+		
+		
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        bUseRTTI = true;
+        bEnableExceptions = true;
 
-		PublicDependencyModuleNames.AddRange(
+        PublicIncludePaths.AddRange(
+            new string[] {
+                    Path.Combine(ModuleDirectory, "Public"),
+            }
+            );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                    Path.Combine(ModuleDirectory, "Private"),
+                    Path.Combine(ModuleDirectory, "Private/internal"),
+                    Path.Combine(ThirdPartyPath, "websocketpp"),
+                    Path.Combine(ThirdPartyPath, "asio/asio/include"),
+                    Path.Combine(ThirdPartyPath, "rapidjson/include"),
+            }
+            );
+
+        PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"Core",
